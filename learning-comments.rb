7af14f0db5c484cc75c77cd645027995e65cf305
Susana vik <susana.m.vik@gmail.com>
2021-01-30 22:14:15 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,24 @@
+#name = "Susana"
+#name = "Luca"
+
+#puts "We're sorry, but per health inspector's rules, #{name}
+#is not allowed in
+#the store"
+
+#puts "code before if...end"
+#if run_code_inside
+  #puts "code inside"
+#end
+#puts "Code after if...end"
+
+#chance_of_rain = -23
+#if chance_of_rain <= 0.25
+#  puts "pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#  puts "Pack an umbrella!"
+#else
+#  puts "Stay home and read Hegel."
+# end
+
+this_year = Time.now.year
+puts "Hey, it's not 2019!" unless this_year == 2019
